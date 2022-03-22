5f66495e9f968b151e0bc439901253e6996a9326
Stefan Crummenerl <crummenerls@gmail.com>
2022-03-22 19:44:33 +0100

    Go annalyns-infiltration

---
+++ go/annalyns-infiltration/annalyns_infiltration.go
@@ -0,0 +1,25 @@
+package annalyn
+
+// You have four tasks: to implement the logic for determining if the above actions are available based on the state of
+// the three characters found in the forest and whether Annalyn's pet dog is present or not.
+
+// CanFastAttack can be executed only when the knight is sleeping
+func CanFastAttack(knightIsAwake bool) bool {
+	return !knightIsAwake
+}
+
+// CanSpy can be executed if at least one of the characters is awake
+func CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake bool) bool {
+	return knightIsAwake || archerIsAwake || prisonerIsAwake
+}
+
+// CanSignalPrisoner can be executed if the prisoner is awake and the archer is sleeping
+func CanSignalPrisoner(archerIsAwake, prisonerIsAwake bool) bool {
+	return prisonerIsAwake && !archerIsAwake
+}
+
+// CanFreePrisoner can be executed if the prisoner is awake and the other 2 characters are asleep
+// or if Annalyn's pet dog is with her and the archer is sleeping
+func CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent bool) bool {
+	return (prisonerIsAwake && !knightIsAwake && !archerIsAwake) || (petDogIsPresent && !archerIsAwake)
+}
